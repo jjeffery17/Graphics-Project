326a2372ac18e03aba220bd8699ff2235aa9467f
James <jjeffery17@heathfieldcc.co.uk>
2025-01-03 17:29:15 +0000

    implemented system to render simple triangles using area summations

--- screen_coords.py
+++ screen_coords.py
@@ -43,6 +43,23 @@ def vector_division(a: list[float], b: float) -> list[float]:
         out.append(item/b)
     return out
 
+def vector_cross(a: [float, float, float], b: [float, float, float]) -> [float, float, float]:
+    o = [0,0,0]
+    if len(a) == 2:
+        a.append(0.0)
+    if len(b) == 2:
+        b.append(0.0)
+    o[0] = a[1]*b[2] - a[2]*b[1]
+    o[1] = a[2]*b[0] - a[0]*b[2]
+    o[2] = a[0]*b[1] - a[1]*b[0]
+    return o
+
+def vector_dot(a: list[float], b: list[float]) -> float:
+    o = 0
+    for i in range(len(a)):
+        o += a[i]*b[i]
+    return o
+
 class World:
     def __init__(self, screenx, screeny, fov=90.0):
         self.screenx = screenx
@@ -80,7 +97,7 @@ class World:
                 if self.point_on_screen([vertex[0], vertex[1], vertex[2]]) != (True, True):
                     points_off_screen += 1
             if points_off_screen < 3:
-                points = self.get_points_in_triangle(points[0], points[1], points[2])#
+                points = self.get_points_in_triangle(points[0], points[1], points[2])
                 for point in points:
                     x = int(point[0])
                     y = int(point[1])
@@ -120,33 +137,26 @@ class World:
         y = (abs(self.screeny * (1 - y)) / self.screenratio) - ((self.screenx - self.screeny) / 2)
         return x, y
     def get_points_in_triangle(self, a: list[float], b: list[float], c: list[float]):
+        max_x = math.ceil(max(a[0], b[0], c[0]))
+        min_x = math.floor(min(a[0], b[0], c[0]))
+        max_y = math.ceil(max(a[1], b[1], c[1]))
+        min_y = math.floor(min(a[1], b[1], c[1]))
+
+        a[2] = 1.0
+        b[2] = 1.0
+        c[2] = 1.0
+
+        tolerance = 0
+
+        area_abc = 0.5*vector_size(vector_cross(vector_subtraction(b, a), vector_subtraction(c, a)))
+
         points = []
-
-        ab = self.get_points_on_line(a, b)
-        ac = self.get_points_on_line(c, a)
-        bc = self.get_points_on_line(b, c)
-        perimeter = unique_list(ab + ac + bc)
-        points += perimeter
-
-        pairs = []
-        for i in range(len(perimeter)):
-            for j in range(i + 1, len(perimeter)):
-                if perimeter[i][0] == perimeter[j][0]:
-                    pairs.append([perimeter[i], perimeter[j]])
-        pairs = unique_list(pairs)
-        for pair in pairs:
-            points += self.get_points_on_line(pair[0], pair[1])
+        for x in range(min_x, max_x + 1):
+            for y in range(min_y, max_y + 1):
+                p = [x, y, 1.0]
+                area_pab = 0.5*vector_size(vector_cross(vector_subtraction(a, p), vector_subtraction(b, p)))
+                area_pbc = 0.5 * vector_size(vector_cross(vector_subtraction(b, p), vector_subtraction(c, p)))
+                area_pca = 0.5 * vector_size(vector_cross(vector_subtraction(c, p), vector_subtraction(a, p)))
+                if area_abc-tolerance <= area_pab+area_pbc+area_pca <= area_abc+tolerance:
+                    points.append(p)
         return points
-    def get_points_on_line(self, a: list[float], b: list[float]):
-        delta = vector_subtraction(b, a)
-        beta = a
-        alpha = vector_division(delta, vector_size(delta))
-
-        s = []
-        for l in range(0, math.floor(vector_size(delta))):
-            r = vector_addition(vector_multiplication(alpha, l), beta)
-            for i in range(len(r)):
-                r[i] = math.ceil(r[i])
-            s.append(r)
-
-        return unique_list(s)
